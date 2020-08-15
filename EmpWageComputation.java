c651d57d091705c73d3e60dacb1cf50b1d8a1f12
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-15 19:21:06 +0530

    [Siva]Added:To check emp wage using case statement

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,27 +1,27 @@
 public class EmpWageComputation
 {
-		public static void main(String[] args)
-		{
-				final int FULLTIME = 1;
-				final int PARTTIME = 0;
+	public static void main(String[] args)
+	{
+		final int FULLTIME = 1;
+		final int PARTTIME = 0;
        		int WAGE_PER_HOUR = 20;
        		int workingHours = 8;
        		int dailyWage = 0;
 
-				int checkWorking = (int) (Math.random() * 3);
+		int checkWorking = (int) (Math.random() * 3);
 
-				switch (checkWorking) {
-						case FULLTIME:
-								workingHours = 8;
-								break;
-						case PARTTIME:
-								workingHours = 4;
-								break;
-						default:
-								workingHours = 0;
-				}
+		switch (checkWorking) {
+			case FULLTIME:
+				workingHours = 8;
+				break;
+			case PARTTIME:
+				workingHours = 4;
+				break;
+			default:
+				workingHours = 0;
+		}
 
-				dailyWage = (WAGE_PER_HOUR * workingHours);
-				System.out.println("Daily Employee Wage:" + dailyWage);
+		dailyWage = (WAGE_PER_HOUR * workingHours);
+		System.out.println("Daily Employee Wage:" + dailyWage);
     	}
 }
