7e742d9a5366dfe94170e028b9056ea6d0282ca7
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-15 20:08:15 +0530

    [Siva]Refactored code to calculate daily wage

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,19 +1,19 @@
 public class EmpWageComputation
 {
 	public static void main(String[] args)
-   {
+   	{
 		int PRESENT = 1;
 		int WAGE_PER_HOUR = 20;
-      int WORKING_HOURS = 8;
-      int dailyWage = 0;
+      		int WORKING_HOURS = 8;
+      		int dailyWage = 0;
 		int checkPresentOrAbsent = (int) (Math.random() * 2); //check employee present or not using random function
 
-      if (checkPresentOrAbsent == PRESENT)
+      		if (checkPresentOrAbsent == PRESENT)
 		{
-      	dailyWage = (WAGE_PER_HOUR * WORKING_HOURS);
-      } else {
-         dailyWage=0;
-      }
-      	System.out.println("Daily Employee Wage:" + dailyWage);
-   }
+      			dailyWage = (WAGE_PER_HOUR * WORKING_HOURS);
+      		} else {
+         		dailyWage=0;
+      		}
+      			System.out.println("Daily Employee Wage:" + dailyWage);
+   	}
 }
