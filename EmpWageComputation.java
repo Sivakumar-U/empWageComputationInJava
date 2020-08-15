2b5efe4d0c19214be6e4ce5d138b005d852c37e5
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-15 19:11:53 +0530

    [Siva]Added:To check employee wage using case statement

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,27 +2,26 @@ public class EmpWageComputation
 {
 		public static void main(String[] args)
 		{
-				int PRESENT = 1;
-       		int FULLTIME = 1;
+				final int FULLTIME = 1;
+				final int PARTTIME = 0;
        		int WAGE_PER_HOUR = 20;
        		int workingHours = 8;
        		int dailyWage = 0;
 
-       		int checkPresentOrAbsent = (int) (Math.random() * 2); //check employee present or not using random function
+				int checkWorking = (int) (Math.random() * 3);
 
-				if (checkPresentOrAbsent == PRESENT)
-				{
-						int checkWorking = (int) (Math.random() * 2);
-
-						if (checkWorking == FULLTIME)
+				switch (checkWorking) {
+						case FULLTIME:
 								workingHours = 8;
-						else
+								break;
+						case PARTTIME:
 								workingHours = 4;
-
-						dailyWage = (WAGE_PER_HOUR * workingHours);
-						System.out.println("Daily Employee Wage:" + dailyWage);
-				} else {
-					System.out.print("Employee is absent");
+								break;
+						default:
+								workingHours = 0;
 				}
+
+				dailyWage = (WAGE_PER_HOUR * workingHours);
+				System.out.println("Daily Employee Wage:" + dailyWage);
     	}
 }
