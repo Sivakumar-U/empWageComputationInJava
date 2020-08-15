19f5d5cff8a2ac926f785a87c336463268b7887a
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-15 18:42:37 +0530

    [Siva]Added:To check employee working full time or part time

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -1,19 +1,30 @@
 public class EmpWageComputation
 {
 	public static void main(String[] args)
-   {
-		int PRESENT = 1;
-		int WAGE_PER_HOUR = 20;
-      int WORKING_HOURS = 8;
-      int dailyWage = 0;
-		int checkPresentOrAbsent = (int) (Math.random() * 2); //check employee present or not using random function
+	{
+       int PRESENT = 1;
+       int FULLTIME = 1;
+       int WAGE_PER_HOUR = 20;
+       int workingHours = 8;
+       int dailyWage = 0;
 
-      if (checkPresentOrAbsent == PRESENT)
-		{
-      	dailyWage = (WAGE_PER_HOUR * WORKING_HOURS);
-      } else {
-         dailyWage=0;
-      }
-      	System.out.println("Daily Employee Wage:" + dailyWage);
-   }
+       int checkPresentOrAbsent = (int) (Math.random() * 2); //check employee present or not using random function
+
+		 if (checkPresentOrAbsent == PRESENT)
+		 {
+				int checkWorking = (int) (Math.random() * 2);
+
+         	if (checkWorking == FULLTIME)
+         			workingHours = 8;
+         	else
+               	workingHours = 4;
+
+         	dailyWage = (WAGE_PER_HOUR * workingHours);
+         	System.out.println("Daily Employee Wage:" + dailyWage);
+       }
+			else
+			{
+            	System.out.print("Employee is absent");
+       	}
+    }
 }
