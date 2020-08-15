1389ef78d48d1e250dc334797ad2a59ff3eb4c00
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-15 20:03:26 +0530

    [Siva]Added:To check employee present or absent

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,37 +2,13 @@ public class EmpWageComputation
 {
 	public static void main(String[] args)
 	{
-		final int FULLTIME = 1;
-		final int PARTTIME = 0;
-		int WORKINGDAYS_PER_MONTH = 20;
-       		int WAGE_PER_HOUR = 20;
-		int TOTALHOURS = 100;
-       		int workingHours = 8;
-       		int monthlyWage = 0;
-		int totalDays = 0;
-		int totalHours = 0;
+    		int PRESENT = 1;
 
-		while (totalDays < WORKINGDAYS_PER_MONTH && totalHours < TOTALHOURS) {
-			totalDays++;
+		int checkPresentOrAbsent = (int) (Math.random() * 2);
 
-			int checkWorking = (int) (Math.random() * 3);
-
-			switch (checkWorking) {
-				case FULLTIME:
-					workingHours = 8;
-					break;
-				case PARTTIME:
-					workingHours = 4;
-					break;
-				default:
-					workingHours = 0;
-			}
-
-			totalHours = totalHours+workingHours;
-		}
-
-		monthlyWage = (WAGE_PER_HOUR * totalHours);
-		System.out.println("Total Hours:" + totalHours);
-		System.out.println("Monthly Employee Wage:" + monthlyWage);
-    	}
+		if (checkPresentOrAbsent == PRESENT)
+			System.out.print("Employee is present");
+		else
+			System.out.println("Employee is absent");
+	}
 }
