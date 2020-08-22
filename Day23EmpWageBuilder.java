cec51fa1443eaa9fffca755f23e7a7dcbace2a09
Sivakumar-U <usivakumarreddy@gmail.com>
2020-08-22 15:05:54 +0530

    [Siva]Added:Total wage of different companies using class obje

--- Day23EmpWageBuilder.java
+++ Day23EmpWageBuilder.java
@@ -1,79 +1,55 @@
 public class Day23EmpWageBuilder
 {
-	//constant
-   	final static int IS_FULL_TIME=1;
-   	final static int IS_PART_TIME=2;
+	private static final int FULL_TIME = 1;
+	private static final int PART_TIME = 0;
 
-	//class variables
-	public int empRate, numberOfDays, numberOfHrs, totalWages;
-	public String company;
+    	private final String companyName;
+    	private final int wagePerHour;
+    	private final int workingDaysPerMonth;
+    	private final int maxHours;
 
-	//constructor
-	public Day23EmpWageBuilder(String companyName,int rate,int days,int hrs)
+    	public Day23EmpWageBuilder(String companyName, int wagePerHour, int workingDaysPerMonth, int maxHours)
 	{
-		this.company=companyName;
-		this.empRate=rate;
-		this.numberOfDays=days;
-		this.numberOfHrs=hrs;
-	}
+        	this.companyName = companyName;
+        	this.wagePerHour = wagePerHour;
+        	this.workingDaysPerMonth = workingDaysPerMonth;
+        	this.maxHours = maxHours;
+    	}
 
-   	//return empHrs
-   	public int getHour()
-   	{
-		int attendence=(int)(Math.random()*10)%3;
-      		int empHrs=0;
-
-      		switch(attendence)
-      		{
-         		case IS_FULL_TIME:
-            			empHrs=8;
-            			break;
-         		case IS_PART_TIME:
-            			empHrs=4;
-            			break;
-         		default:
-            			empHrs=0;
-            			break;
-      		}
-      		return empHrs;
-   	}
-
-   	//return daily empWage
-   	public int getDailyWage(int hrs)
-   	{
-      		return (hrs*empRate);
-   	}
-
-	//compute employ wage
-	public void computeEmployWage()
+    	public void get_EmployeeWageForCompany()
 	{
-		int totalWorkingDay=0, totalWorkingHrs=0 , hours;
+        	int totalWage = 0;
+        	int dailyWage = 0;
+        	int totalDays = 0;
+        	int totalHours = 0;
+        	int workingHours = 0;
 
-		//find totalWorkingHrs
-		while(totalWorkingHrs<numberOfHrs && totalWorkingDay<numberOfDays)
-      		{
-			hours=getHour();
-         		totalWorkingHrs=totalWorkingHrs+hours;
-			System.out.println(company+" Employee Wage for day "+(++totalWorkingDay)+" : "+getDailyWage(hours));
-      		}
-		//calculate total wage
-		totalWages=getDailyWage(totalWorkingHrs);
-		System.out.println();
-	}
+        	while (totalDays <= workingDaysPerMonth && totalHours < maxHours)
+		{
+            		totalDays++;
+            		int checkWorking = (int) (Math.random() * 3);
+            		switch (checkWorking)
+			{
+                		case FULL_TIME:
+                    			workingHours = 8;
+                    			break;
+                		case PART_TIME:
+                    			workingHours = 4;
+                    			break;
+                		default:
+                    			workingHours = 0;
+            		}
+            		dailyWage = (wagePerHour * workingHours);
+            		System.out.println("Day " + totalDays + " Employee Wage for Company " + companyName + " is " + dailyWage);
+            		totalWage += dailyWage;
+        	}
+        	System.out.println("Total Employee Wage for " + companyName + " is " + totalWage);
+    	}
 
-	public void totalEmployWage()
+    	public static void main(String[] args)
 	{
-		System.out.println(company+" Total Employee Wage for month : "+ totalWages);
-	}
-
-	public static void main(String args[])
-   	{
-		//create instance
-      		Day23EmpWageBuilder jio= new Day23EmpWageBuilder("Relance jio",20,10,100);
-      		Day23EmpWageBuilder tcs= new Day23EmpWageBuilder("TCS",15,10,100);
-      		jio.computeEmployWage();
-      		tcs.computeEmployWage();
-		jio.totalEmployWage();
-		tcs.totalEmployWage();
-   	}
+        	System.out.println("Welcome to Employee Wage Computation");
+       	 	Day23EmpWageBuilder employeeWageBuilder = new Day23EmpWageBuilder("Reliance", 20, 20, 50);
+        	employeeWageBuilder.get_EmployeeWageForCompany();
+    	}
 }
